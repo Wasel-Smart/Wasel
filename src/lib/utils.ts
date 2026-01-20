c0a0f19f55cbe1899d748a6f28299858f0ec908b
Wasel-01 <243191245+Wasel-Smart@users.noreply.github.com>
2026-01-20 19:10:16 +0300

    yes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,5 @@
 import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { twMerge } from "tailwind-merge/dist/bundle-mjs.mjs"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
